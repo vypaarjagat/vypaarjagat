31cd830d228b3ef79792b6f12b95aaf1211cf1d7
vypaarjagat <tanishka.enterprises0620199@gmail.com>
2026-03-28 23:44:50 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,25 @@
+const router = require('express').Router();
+const Product = require('../models/Product');
 const auth = require('../middleware/auth');
 
+// CREATE PRODUCT (Seller only)
 router.post('/', auth, async (req, res) => {
-  // only logged user
+  if (req.user.role !== 'seller')
+    return res.status(403).json({ msg: "Only seller allowed" });
+
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price
+  });
+
+  await product.save();
+  res.json(product);
 });
+
+// GET PRODUCTS
+router.get('/', async (req, res) => {
+  const data = await Product.find();
+  res.json(data);
+});
+
+module.exports = router;
