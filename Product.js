0defac1317c562b1cb5eb6067e30860de6fb55aa
vypaarjagat <tanishka.enterprises0620199@gmail.com>
2026-03-28 22:14:25 +0530

    Delete Product.js

--- Product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  sellerId: String
-});
-
-module.exports = mongoose.model('Product', schema);
