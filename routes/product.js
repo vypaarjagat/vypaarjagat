30346e2328a20eb2fe8bab196fdaa2e2d19b261d
vypaarjagat <tanishka.enterprises0620199@gmail.com>
2026-03-28 23:01:53 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
-const Product = require('../models/Product');
 
-router.post('/', async (req, res) => {
-  const product = await Product.create(req.body);
-  res.json(product);
+// Temporary dummy data
+router.get('/', async (req, res) => {
+  res.json([
+    { name: "Product 1", price: 100 },
+    { name: "Product 2", price: 200 }
+  ]);
 });
 
-router.get('/', async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+router.post('/', async (req, res) => {
+  res.json({ message: "Product added (dummy)" });
 });
 
 module.exports = router;
